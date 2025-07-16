2e98009cabddf0ceb6b4d0e89f251ed7b2f126e3
Harsh vardhan <109621041+harsh89u798@users.noreply.github.com>
2025-07-17 01:06:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.getElementById('loginForm').addEventListener('submit', e=>{
+document.getElementById('loginForm').addEventListener('submit', e => {
   e.preventDefault();
-  const user={
-    username:username.value.trim(),
-    email:email.value.trim(),
-    phone:phone.value.trim(),
-    pwd:password.value
+  const user = {
+    username: username.value.trim(),
+    email: email.value.trim(),
+    phone: phone.value.trim(),
+    pwd: password.value
   };
-  localStorage.setItem('djUser',JSON.stringify(user));
-  if(!localStorage.getItem('djEntries')) localStorage.setItem('djEntries','[]');
-  location='journal.html';
+  localStorage.setItem('djUser', JSON.stringify(user));
+  if (!localStorage.getItem('djEntries')) localStorage.setItem('djEntries', '[]');
+  location = 'journal.html';
 });
+
 
